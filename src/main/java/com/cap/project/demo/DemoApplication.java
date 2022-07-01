f1062589112d187ea3447002c0c1b2a0c63c5943
soochangoforit <tncksdl05@gmail.com>
2022-07-01 22:06:19 +0900

    feat : add hidden filter method of api
    can use delete or update method on form data

--- src/main/java/com/cap/project/demo/DemoApplication.java
+++ src/main/java/com/cap/project/demo/DemoApplication.java
@@ -2,12 +2,23 @@ package com.cap.project.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	//@DeleteMapping, @PutMapping 등의 최신 매핑기능을 이용하기 위해선 아래와 같은 코드를 작성해야 한다고 한다.
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+		return new HiddenHttpMethodFilter();
+	}
+
 
 }
