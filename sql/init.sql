b20363139c82cd95fd848737476312ad619bbafd
soochan <tncksdl05@gmail.com>
2022-07-11 15:42:09 +0900

    feat(JS) : add js for showing alret message
    add js for showing alret message when client withdrawal or logout

--- sql/init.sql
+++ sql/init.sql
@@ -1,2 +1,7 @@
-insert into departments values ( 1 , '심리상담')
+insert into departments values ( 1 , '정신분석상담');
+insert into departments values ( 2 , '개인심리상담');
+insert into departments values ( 3 , '분석심리상담');
+insert into departments values ( 4 , '가정폭력상담');
+insert into departments values ( 5 , '아동발달상담');
+
 
